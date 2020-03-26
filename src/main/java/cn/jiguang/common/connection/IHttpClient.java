1be56e82d5629000770765d98b6a157670630521
xudx <xudx@jiguang.cn>
2020-03-26 17:01:36 +0800

    新增文件API相关配置

--- src/main/java/cn/jiguang/common/connection/IHttpClient.java
+++ src/main/java/cn/jiguang/common/connection/IHttpClient.java
@@ -13,21 +13,21 @@ public interface IHttpClient {
     public static final String CHARSET = "UTF-8";
     public static final String CONTENT_TYPE_JSON = "application/json";
     public static final String CONTENT_TYPE_FORM = "application/x-www-form-urlencoded";
-    
+
     public static final String RATE_LIMIT_QUOTA = "X-Rate-Limit-Limit";
     public static final String RATE_LIMIT_Remaining = "X-Rate-Limit-Remaining";
     public static final String RATE_LIMIT_Reset = "X-Rate-Limit-Reset";
     public static final String JPUSH_USER_AGENT = "JPush-API-Java-Client";
-    
+
     public static final int RESPONSE_OK = 200;
-    
+
     public enum RequestMethod {
-        GET, 
+        GET,
         POST,
         PUT,
         DELETE
     }
-    
+
     public static final String IO_ERROR_MESSAGE = "Connection IO error. \n"
             + "Can not connect to JPush Server. "
             + "Please ensure your internet connection is ok. \n"
@@ -49,27 +49,27 @@ public interface IHttpClient {
 
     //设置连接超时时间
     public static final int DEFAULT_CONNECTION_TIMEOUT = (5 * 1000); // milliseconds
-    
+
     //设置读取超时时间
     public static final int DEFAULT_READ_TIMEOUT = (30 * 1000); // milliseconds
-    
+
     public static final int DEFAULT_MAX_RETRY_TIMES = 3;
 
-    public ResponseWrapper sendGet(String url) 
+    public ResponseWrapper sendGet(String url)
             throws APIConnectionException, APIRequestException;
 
     public ResponseWrapper sendGet(String url, String content)
             throws APIConnectionException, APIRequestException;
-    
-    public ResponseWrapper sendDelete(String url) 
+
+    public ResponseWrapper sendDelete(String url)
             throws APIConnectionException, APIRequestException;
 
     public ResponseWrapper sendDelete(String url, String content)
             throws APIConnectionException, APIRequestException;
-    
-    public ResponseWrapper sendPost(String url, String content) 
+
+    public ResponseWrapper sendPost(String url, String content)
             throws APIConnectionException, APIRequestException;
-    
+
 
     public ResponseWrapper sendPut(String url, String content)
             throws APIConnectionException, APIRequestException;
