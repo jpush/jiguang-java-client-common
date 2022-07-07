82eaf2db8875da39247aa03766393d790f9126a5
wicked-tc130 <wicked.tc130@foxmail.com>
2022-07-07 18:35:06 +0800

    Update TimeUtils.java
    fix：方法内创建SimpleDateFormat

--- src/main/java/cn/jiguang/common/utils/TimeUtils.java
+++ src/main/java/cn/jiguang/common/utils/TimeUtils.java
@@ -1,32 +1,15 @@
 package cn.jiguang.common.utils;
 
-
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
 
 public class TimeUtils {
 
-	private static final ThreadLocal<SimpleDateFormat> DATE_FORMAT = new ThreadLocal<SimpleDateFormat>() {
-        @Override
-        protected SimpleDateFormat initialValue() {
-            return new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
-        }
-    };
-    private static final ThreadLocal<SimpleDateFormat> TIME_ONLY_FORMAT = new ThreadLocal<SimpleDateFormat>() {
-        @Override
-        protected SimpleDateFormat initialValue() {
-            return new SimpleDateFormat("HH:mm:ss");
-        }
-    };
-
-    static {
-        DATE_FORMAT.get().setLenient(false);
-        TIME_ONLY_FORMAT.get().setLenient(false);
-    }
-
     public static boolean isDateFormat(String time) {
         try {
-            DATE_FORMAT.get().parse(time);
+            SimpleDateFormat simpleDateFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+            simpleDateFormat.setLenient(false);
+            simpleDateFormat.parse(time);
         } catch (ParseException e) {
             return false;
         }
@@ -34,8 +17,10 @@ public class TimeUtils {
     }
 
     public static boolean isTimeFormat(String time) {
-        try{
-            TIME_ONLY_FORMAT.get().parse(time);
+        try {
+            SimpleDateFormat simpleDateFormat = new SimpleDateFormat("HH:mm:ss");
+            simpleDateFormat.setLenient(false);
+            simpleDateFormat.parse(time);
         } catch (ParseException e) {
             return false;
         }
